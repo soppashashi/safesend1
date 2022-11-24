535bdd0a5ac8a80a842e59731aedad31909b777b
Shashi Soppa <shashi.soppa@vits.co.uk>
2022-11-24 08:28:31 +0000

    first commit

---
+++ ClientApp/build/precache-manifest.4023376c88fef1e6cec525b16d65f480.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec84641ba64d386acd3e8e471371f820",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ceed17a0b6197a95511",
+    "url": "/static/css/2.e36f5e60.chunk.css"
+  },
+  {
+    "revision": "8ceed17a0b6197a95511",
+    "url": "/static/js/2.e67983b3.chunk.js"
+  },
+  {
+    "revision": "2a0cc7b29420fe9d65f7cad1c233d0cd",
+    "url": "/static/js/2.e67983b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d5c32cd3262af4fea0",
+    "url": "/static/js/main.855e849f.chunk.js"
+  },
+  {
+    "revision": "4d9972025d17d3e3a5fc",
+    "url": "/static/js/runtime-main.edb469e3.js"
+  }
+]);
